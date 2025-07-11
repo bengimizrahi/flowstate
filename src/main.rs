70d9bbbc5df957e8c37184b1941a1953a7a6d441
Bengi Mizrahi <bengi.mizrahi@argela.com.tr>
2025-07-11 17:06:38 +0300

    create, delete teams and undo/redo functionality

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 mod support;
+mod flowstate;
 
 fn main() {
+    let _flow_state = flowstate::FlowState::new();
+    
     support::simple_init(file!(), move |_, _ui| {
         // nothing! don't actually do any imgui funtimes
     });
