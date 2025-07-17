96af94235def7b1d13af0f9c93f10d7e09858ced
Bengi Mizrahi <bengi.mizrahi@argela.com.tr>
2025-07-17 15:01:55 +0300

    Finalize initialization of main GUI template

--- src/gui.rs
+++ src/gui.rs
@@ -38,13 +38,13 @@ impl Gui {
 
     fn draw(&mut self, ui: &Ui) {
         self.draw_menu_bar(ui);
-        self.draw_top_ribbon(ui);
-        self.draw_gantt_chart(ui);
+        self.draw_ribbon(ui);
+        self.draw_tab_bar(ui);
     }
 
     fn draw_menu_bar(&mut self, ui: &Ui) {
-        ui.menu_bar(|| {
-            ui.menu("File", || {
+        if let Some(menu_bar) = ui.begin_menu_bar() {
+            if let Some(file_menu) = ui.begin_menu("File") {
                 if ui.menu_item("New Project...") {
 
                 }
@@ -57,35 +57,41 @@ impl Gui {
                 if ui.menu_item("Exit") {
 
                 }
-            });
-            ui.menu("Edit", || {
+                file_menu.end();
+            };
+            if let Some(edit_menu) = ui.begin_menu("Edit") {
                 if ui.menu_item("Undo") {
 
                 }
                 if ui.menu_item("Redo") {
 
                 }
-            });
-            ui.menu("Action", || {
+                edit_menu.end();
+            };
+            if let Some(action_menu) = ui.begin_menu("Action") {
                 if ui.menu_item("Team") {
 
                 }
-            });
-            ui.menu("Filter", || {
-            });
-            ui.menu("Help", || {
+                action_menu.end();
+            };
+            if let Some(filter_menu) = ui.begin_menu("Filter") {
+                filter_menu.end();
+            };
+            if let Some(help_menu) = ui.begin_menu("Help") {
                 if ui.menu_item("About") {
 
                 }
-            });
-        });
+                help_menu.end();
+            };
+            menu_bar.end();
+        };
     }
 
-    fn draw_top_ribbon(&mut self, ui: &Ui) {
+    fn draw_ribbon(&mut self, ui: &Ui) {
 
     }
 
-    fn draw_gantt_chart(&mut self, ui: &Ui) {
+    fn draw_tab_bar(&mut self, ui: &Ui) {
         if let Some(_tab_bar) = ui.tab_bar("##tab_bar") {
             if let Some(_res_tab_item) = ui.tab_item("Resources"){
                 self.draw_resources_gantt_chart(ui);
@@ -96,14 +102,8 @@ impl Gui {
         }
     }
 
-    fn draw_resources_gantt_chart(&mut self, ui: &Ui) {
-        self.draw_resources_gantt_chart_header_row(ui);
-        self.draw_milestones_row(ui);
-        self.draw_resources_gantt_chart_contents(ui);
-    }
-
-    fn draw_resources_gantt_chart_header_row(&mut self, ui: &Ui) {
-        let table_id = std::ffi::CString::new("##resources_table").unwrap();
+    fn draw_gantt_chart_table(&mut self, ui: &Ui, id: &str) -> bool {
+        let table_id = std::ffi::CString::new(id).unwrap();
         let flags = imgui::sys::ImGuiTableFlags_Borders
             | imgui::sys::ImGuiTableFlags_HighlightHoveredColumn
             | imgui::sys::ImGuiTableFlags_SizingFixedFit
@@ -112,51 +112,62 @@ impl Gui {
             | imgui::sys::ImGuiTableFlags_Resizable
             | imgui::sys::ImGuiTableFlags_NoPadOuterX
             | imgui::sys::ImGuiTableFlags_NoPadInnerX;
-        if unsafe {imgui::sys::igBeginTable(
+        unsafe {imgui::sys::igBeginTable(
             table_id.as_ptr(),
             (self.flow_state.l1().num_days() + 1) as i32,
             flags as i32,
             imgui::sys::ImVec2 { x: 0.0, y: 0.0 },
             0.0,
-        )} {
-            let mut table_data = TableColumnSetup::new("Calendar");
-            table_data.flags = TableColumnFlags::NO_HIDE | TableColumnFlags::NO_REORDER;
-            ui.table_setup_column_with(table_data);
-            for i in 0..self.flow_state.l1().num_days() {
-                let day: chrono::NaiveDate = self.flow_state.l1().start_date + chrono::Duration::days(i as i64);
-                let day_str = day.format("%m/%d").to_string();
-                let day_cstr = std::ffi::CString::new(day_str).unwrap();
-                unsafe {imgui::sys::igTableSetupColumn(
-                    day_cstr.as_ptr(),
-                    (imgui::sys::ImGuiTableColumnFlags_AngledHeader
-                        | imgui::sys::ImGuiTableColumnFlags_WidthFixed
-                        | imgui::sys::ImGuiTableColumnFlags_NoResize) as i32,
-                    0.0,
-                    0,
-                );}
-            }
-            unsafe {imgui::sys::igTableSetupScrollFreeze(1, 4);}
-            unsafe {imgui::sys::igTableAngledHeadersRow();}
-            ui.table_headers_row();
+        )}
+    }
+    
+    fn draw_resources_gantt_chart(&mut self, ui: &Ui) {
+        if self.draw_gantt_chart_table(ui, "##resources_gantt_chart") {
+            self.draw_gantt_chart_header_row(ui);
+            self.draw_milestones_row(ui);
+            self.draw_resources_gantt_chart_contents(ui);
+            unsafe {imgui::sys::igEndTable();}
+        }
+    }
 
-            let today = chrono::Local::now().date_naive();
-            for i in 0..self.flow_state.l1().num_days() {
-                let day: chrono::NaiveDate = self.flow_state.l1().start_date + chrono::Duration::days(i as i64);
-                if (day == today) {
-                    let pink = [1.0, 0.75, 0.8, 1.0];
-                    ui.table_set_bg_color_with_column(TableBgTarget::CELL_BG, pink, i + 1);
-                }
+    fn draw_tasks_tab_gantt_chart(&mut self, ui: &Ui) {
+        if self.draw_gantt_chart_table(ui, "##tasks_gantt_chart") {
+            self.draw_gantt_chart_header_row(ui);
+            self.draw_milestones_row(ui);
+            self.draw_tasks_gantt_chart_contents(ui);
+            unsafe {imgui::sys::igEndTable();}
+        }
+    }
+
+    fn draw_gantt_chart_header_row(&mut self, ui: &Ui) {
+        let mut table_data = TableColumnSetup::new("Calendar");
+        table_data.flags = TableColumnFlags::NO_HIDE | TableColumnFlags::NO_REORDER;
+        ui.table_setup_column_with(table_data);
+        for i in 0..self.flow_state.l1().num_days() {
+            let day: chrono::NaiveDate = self.flow_state.l1().start_date + chrono::Duration::days(i as i64);
+            let day_str = day.format("%m/%d").to_string();
+            let day_cstr = std::ffi::CString::new(day_str).unwrap();
+            unsafe {imgui::sys::igTableSetupColumn(
+                day_cstr.as_ptr(),
+                (imgui::sys::ImGuiTableColumnFlags_AngledHeader
+                    | imgui::sys::ImGuiTableColumnFlags_WidthFixed
+                    | imgui::sys::ImGuiTableColumnFlags_NoResize) as i32,
+                0.0,
+                0,
+            );}
+        }
+        unsafe {imgui::sys::igTableSetupScrollFreeze(1, 4);}
+        unsafe {imgui::sys::igTableAngledHeadersRow();}
+        ui.table_headers_row();
+
+        let today = chrono::Local::now().date_naive();
+        for i in 0..self.flow_state.l1().num_days() {
+            let day: chrono::NaiveDate = self.flow_state.l1().start_date + chrono::Duration::days(i as i64);
+            if day == today {
+                let pink = [1.0, 0.75, 0.8, 1.0];
+                ui.table_set_bg_color_with_column(TableBgTarget::CELL_BG, pink, i + 1);
             }
         }
-
-        unsafe {imgui::sys::igEndTable();}
-    }
-
-    fn draw_resources_gantt_chart_contents(&mut self, ui: &Ui) {
-
-    }
-    fn draw_tasks_tab_gantt_chart(&mut self, ui: &Ui) {
-
     }
 
     fn draw_milestones_row(&mut self, ui: &Ui) {
@@ -166,4 +177,11 @@ impl Gui {
         for i in 0..self.flow_state.l1().num_days() {
         }
     }
+
+    fn draw_resources_gantt_chart_contents(&mut self, ui: &Ui) {
+    }
+
+    fn draw_tasks_gantt_chart_contents(&mut self, ui: &Ui) {
+    }
+
 }
