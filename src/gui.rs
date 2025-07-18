189259b76b22dda0356ffcf45139156590533cf0
Bengi Mizrahi <bengi.mizrahi@argela.com.tr>
2025-07-18 17:04:52 +0300

    Implement Create/Rename/Delete resource menu

--- src/gui.rs
+++ src/gui.rs
@@ -1,4 +1,5 @@
 use imgui::*;
+use crate::flowstate::Resource;
 use crate::support;
 use crate::flowstate;
 use chrono::Datelike;
@@ -7,6 +8,7 @@ pub struct Gui {
     flow_state: flowstate::state::FlowState,
 
     team_input_text_buffer: String,
+    resource_input_text_buffer: String,
     milestone_input_text_buffer: String,
     milestone_date_input_text_buffer: String,
 }
@@ -18,6 +20,7 @@ impl Gui {
         Gui {
             flow_state,
             team_input_text_buffer: String::new(),
+            resource_input_text_buffer: String::new(),
             milestone_input_text_buffer: String::new(),
             milestone_date_input_text_buffer: String::new(),
         }
@@ -278,11 +281,11 @@ impl Gui {
     fn draw_resources_gantt_chart_contents(&mut self, ui: &Ui) {
         let team_names: Vec<String> = self.flow_state.teams.iter().cloned().collect();
         for team_name in team_names.iter() {
-            self.draw_resource_gantt_chart_team(ui, team_name);
+            self.draw_resources_gantt_chart_team(ui, team_name);
         }
     }
 
-    fn draw_resource_gantt_chart_team(&mut self, ui: &Ui, team_name: &str) {
+    fn draw_resources_gantt_chart_team(&mut self, ui: &Ui, team_name: &str) {
         ui.table_next_row();
         ui.table_next_column();
         
@@ -305,6 +308,11 @@ impl Gui {
         }
 
         if expand_team {
+            let resources: Vec<String> = self.flow_state.l1().team_members.get(team_name)
+                .unwrap_or(&vec![]).clone();
+            for resource_name in resources.iter() {
+                self.draw_resources_gantt_chart_team_resource(ui, resource_name);
+            }
             unsafe {imgui::sys::igTreePop();}
         }
 
@@ -338,10 +346,96 @@ impl Gui {
             if ui.menu_item("Delete Team") {
                 self.flow_state.delete_team(team_name).unwrap();
             }
+            ui.separator();
+            if let Some(_create_resource_menu) = ui.begin_menu("Create Resource") {
+                if let Some(child_window) = ui.child_window("##create_resource_menu")
+                        .size([140.0, 20.0])
+                        .begin() {
+                    let mut can_create_resource = false;
+                    if ui.input_text("##resource_name", &mut self.resource_input_text_buffer)
+                            .enter_returns_true(true)
+                            .hint("Enter resource name")
+                            .build() {
+                        can_create_resource = !self.resource_input_text_buffer.is_empty();
+                    }
+                    ui.same_line();
+                    if ui.button("Ok") {
+                        can_create_resource = !self.resource_input_text_buffer.is_empty();
+                    }
+                    if can_create_resource {
+                        ui.close_current_popup();
+                        self.flow_state.create_resource(Resource {
+                            name: self.resource_input_text_buffer.clone(),
+                            team_name: team_name.to_string(),
+                        }).unwrap_or_else(|e| {
+                            eprintln!("Failed to create resource: {e}");
+                        });
+                        self.resource_input_text_buffer.clear();
+                    }
+                    child_window.end();
+                }
+            }
             popup.end();
         }
     }
 
+    fn draw_resources_gantt_chart_team_resource(&mut self, ui: &Ui, resource_name: &str) {
+        ui.table_next_row();
+        ui.table_next_column();
+        let resource_name_cstr = std::ffi::CString::new(resource_name).unwrap();
+        let flags = imgui::sys::ImGuiTreeNodeFlags_SpanFullWidth | imgui::sys::ImGuiTreeNodeFlags_DefaultOpen;
+        let expand_resource = unsafe {
+            imgui::sys::igTreeNodeEx_Str(resource_name_cstr.as_ptr(), flags as i32)
+        };
+        self.draw_popup_context_menu_for_team_resource_header_column(ui, resource_name);
+
+        for i in 1..=self.flow_state.l1().num_days() {
+            if ui.table_next_column() {
+                let _id = ui.push_id_usize(i);
+                let day = self.flow_state.l1().day(i - 1);
+                if day.weekday() == chrono::Weekday::Sat || day.weekday() == chrono::Weekday::Sun {
+                    let bg_color = ui.style_color(StyleColor::TableHeaderBg);
+                    ui.table_set_bg_color(TableBgTarget::CELL_BG, bg_color);
+                }
+            }
+        }
+        if expand_resource {
+            unsafe {imgui::sys::igTreePop();}
+        }
+    }
+
+    fn draw_popup_context_menu_for_team_resource_header_column(&mut self, ui: &Ui, resource_name: &str) {
+        if let Some(popup) = ui.begin_popup_context_item() {
+            if let Some(_rename_resource_menu) = ui.begin_menu("Rename Resource") {
+                if let Some(child_window) = ui.child_window("##rename_resource_menu")
+                        .size([140.0, 20.0])
+                        .begin() {
+                    let mut can_create_resource = false;
+                    if ui.input_text("##new_resource_name", &mut self.resource_input_text_buffer)
+                            .enter_returns_true(true)
+                            .hint(resource_name)
+                            .build() {
+                        can_create_resource = !self.resource_input_text_buffer.is_empty();
+                    }
+                    ui.same_line();
+                    if ui.button("Ok") {
+                        can_create_resource = !self.resource_input_text_buffer.is_empty();
+                    }
+                    if can_create_resource {
+                        ui.close_current_popup();
+                        self.flow_state.rename_resource(resource_name, &self.resource_input_text_buffer).unwrap();
+                        self.resource_input_text_buffer.clear();
+                    }
+                    child_window.end();
+                }
+            }
+            if ui.menu_item("Delete Resource") {
+                self.flow_state.delete_resource(resource_name).unwrap();
+            }
+            popup.end();
+        }
+    }
+    
     fn draw_tasks_gantt_chart_contents(&mut self, ui: &Ui) {
     }
 
